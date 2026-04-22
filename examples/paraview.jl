ab2698949f6517a676d018ec960b2380efc76d15
Michał Mika <michal@mika.sh>
2026-04-22 18:41:35 +0200

    feat: example for unstructured line meshes

--- examples/paraview.jl
+++ examples/paraview.jl
@@ -1,5 +1,38 @@
 using FEMLab
 
+## unstructured line mesh
+
+# columns: node indices, rows: x- and y-coordinates
+nodes = [ 0  10  10
+          0  0   10 ]
+
+# columns: element indices, rows: node indices
+elements = [ 1  2  1
+             2  3  3 ]
+
+# columns: element indices
+bar_youngs_modulus = [ 50, 50, 100 ]
+bar_area = [2, 1, 2*sqrt(2)]
+
+# number of nodes
+nnodes = size(nodes, 2)
+
+# number of elements
+nelems = size(elements, 2)
+
+# unstructured paraview output
+points = vcat(nodes, zeros(nnodes)') # adds third row with zero z-coordinate
+cells = [MeshCell(VTKCellTypes.VTK_LINE, elements[:, k]) for k in 1:nelem ]
+vtk_grid(joinpath("results", "unstructured_line_mesh"), points, cells) do vtk
+    vtk["Young's modulus", VTKCellData()] = bar_youngs_modulus
+    vtk["Area", VTKCellData()] = bar_area
+    vtk["Coordinates", VTKPointData()] = points
+    vtk["Element Indices", VTKCellData()] = 1:nelems
+end
+
+
+
+
 ## unstructured quadrilateral mesh
 
 # columns: node indices, rows: x- and y-coordinates
@@ -26,8 +59,8 @@ nelems = size(elements, 2)
 points = vcat(nodes, zeros(nnodes)') # adds third row with zero z-coordinate
 cells = [MeshCell(VTKCellTypes.VTK_LAGRANGE_QUADRILATERAL, elements[:, k]) for k in 1:nelems]
 vtk_grid(joinpath("results", "unstructured_quad_mesh"), points, cells) do vtk
-    vtk["coordinates"] = points
-    vtk["element_indices"] = 1:nelems
+    vtk["Coordinates"] = points
+    vtk["Element Indices"] = 1:nelems
 end
 
 
@@ -58,7 +91,7 @@ nelems = size(elements, 2)
 points = vcat(nodes, zeros(nnodes)') # adds third row with zero z-coordinate
 cells = [MeshCell(VTKCellTypes.VTK_LAGRANGE_TRIANGLE, elements[:, k]) for k in 1:nelems]
 vtk_grid(joinpath("results", "unstructured_triangular_mesh"), points, cells) do vtk
-    vtk["coordinates", VTKPointData()] = points
-    vtk["element_indices", VTKCellData()] = 1:nelems
+    vtk["Coordinates", VTKPointData()] = points
+    vtk["Element Indices", VTKCellData()] = 1:nelems
 end
 
